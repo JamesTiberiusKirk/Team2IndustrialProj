16f34995be436648bc8d192a124bfa0a21bc8f38
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-10-09 16:28:40 +0100

    Created the middleware that checks the user id and the room id

--- api/src/db/db.ts
+++ api/src/db/db.ts
@@ -1,4 +1,4 @@
-import { createConnection, Connection, ConnectionOptions } from 'mysql';
+import { createConnection, Connection, ConnectionOptions, RowDataPacket } from 'mysql';
 
 export class Db {
     conn: Connection;
@@ -23,6 +23,23 @@ export class Db {
                 resolve('Db Connected');
             });
         });
-
     }
+
+    /**
+     * SQL Function Call.
+     * Checks if the user is in the room.
+     * @param userid id of the user
+     * @param roomid id of the room
+     * @returns boolean
+     */
+    checkUserInRoom(userid: string, roomid: string): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            const sql: string = 'SELECT check_user_in_room(?,?) AS is_in_room;';
+            this.conn.query(sql, [roomid, userid], (err, rows: RowDataPacket[]) => {
+                if (err) reject(err);
+                resolve(Boolean(rows[0]['is_in_room']));
+            });
+        });
+    }
+
 }
