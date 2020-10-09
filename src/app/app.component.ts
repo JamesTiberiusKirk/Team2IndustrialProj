cac97d99c748a0b135567c280c3ff3c20a57f1f7
Christopher Carr <CYCarr@dundee.ac.uk>
2020-10-09 14:26:17 +0100

    changed file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Test Program yeah!';
+  title = 'QuizApp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ScoreComponent } from './score/score.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { LobbyComponent } from './lobby/lobby.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScoreComponent,
-    HeroesComponent
+    LobbyComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
