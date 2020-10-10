5c4eeb440ce75a748837892760f3ecc8ce94bb99
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-10-10 21:34:36 +0100

    Added extra interfaces for answer, responce and idex

--- api/src/models/question.model.ts
+++ api/src/models/question.model.ts
@@ -2,3 +2,18 @@ export interface Question {
     id: string;
     text: string;
 }
+
+export interface Answer {
+    id: string;
+    text: string;
+}
+
+export interface QuestionAndAnswerResponse {
+    question: Question;
+    answers: Answer[];
+}
+
+export interface QuestionIndex {
+    outOf: string;
+    index: string;
+}
