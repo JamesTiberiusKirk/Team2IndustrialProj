3161314f9383d57a2fd643c558cdb40851c5fbf5
fpavlica <33609416+fpavlica@users.noreply.github.com>
2020-10-16 19:27:40 +0200

    Fixed REST service (#80)
    * fixing rest service, adding scores, temporary api test page
    
    * fixed REST service
    
    * javascript is a bad langauge
    
    * removed example usage console.logs
    
    This is just a fix of #79 not getting built
    
    Co-authored-by: fpavlica <frpavlica@gmail.com>

--- web/src/app/api_test/test.component.ts
+++ web/src/app/api_test/test.component.ts
@@ -19,34 +19,35 @@ export class TestComponent {
   
   testRegister() {
     this.rest.register("test335").subscribe((data) => {
-      console.log(data);
-      console.log("the user id is " + data.user_id)
-      //the String() part is necessary. I don't know why but it is.
+      // console.log(data);
+      // console.log("the user id is " + data.user_id)
+      
+      // the String() part is necessary. I don't know why but it is.
       this.userID = String(data.user_id);
     })
   }
 
   testCreateRoom() {
     this.rest.createRoom(this.userID).subscribe((data) => {
-      console.log(data);
-      console.log("the room key is " + data.room_key)
+      // console.log(data);
+      // console.log("the room key is " + data.room_key)
       this.roomKey = String(data.room_key);
     })
   }
 
   testJoinRoom() {
     this.rest.joinRoom(this.userID, this.roomKey).subscribe((data) => {
-      console.log(data);
-      console.log("the room ID is " + data.room_id)
+      // console.log(data);
+      // console.log("the room ID is " + data.room_id)
       this.roomID = String(data.room_id);
     })
   }
 
   testNextQuestion() {
     this.rest.getNextQuestion(this.roomID, this.userID).subscribe((data)=>{
-      console.log(data);
-      console.log("the question ID is " + data.question.id + ", q: " + data.question.text);
-      console.log("the first answer ID is " + data.answers[0].id + ", a: " + data.answers[0].text);
+      // console.log(data);
+      // console.log("the question ID is " + data.question.id + ", q: " + data.question.text);
+      // console.log("the first answer ID is " + data.answers[0].id + ", a: " + data.answers[0].text);
 
       this.questionID = String(data.question.id);
       this.answer1ID = String(data.answers[0].id);
@@ -56,15 +57,15 @@ export class TestComponent {
   testPostAnswer() {
     // just sends the first answer as the chosen
     this.rest.postAnswer(this.roomID, this.userID, this.questionID, this.answer1ID).subscribe((data) => {
-      console.log(data);
-      console.log("the answer was " + (data.correct ? "correct" : "incorrect"));
+      // console.log(data);
+      // console.log("the answer was " + (data.correct ? "correct" : "incorrect"));
     })
   }
 
   testGetScores() {
     this.rest.getScores(this.roomID, this.userID).subscribe((data) => {
-      console.log(data);
-      console.log("the highest score was " + data.scores[0].score + " by " + data.scores[0].nick);
+      // console.log(data);
+      // console.log("the highest score was " + data.scores[0].score + " by " + data.scores[0].nick);
     })
   }
 
