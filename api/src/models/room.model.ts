cf293b5d656f4972754ae1c32fa7f98616585ed4
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-10-18 16:01:28 +0100

    Added jsdocs for models (#84)
    * Added jsdocs for models
    
    * Update api/src/models/question.model.ts
    
    Co-authored-by: fpavlica <33609416+fpavlica@users.noreply.github.com>
    
    Co-authored-by: fpavlica <33609416+fpavlica@users.noreply.github.com>

--- api/src/models/room.model.ts
+++ api/src/models/room.model.ts
@@ -1,3 +1,6 @@
+/**
+ * Interface for room information rest response.
+ */
 export interface RoomResponse {
     room_id: string;
     room_key: string;
--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -1,3 +1,6 @@
+/**
+ * Interface for user information in a rest response.
+ */
 export interface UserResponse {
     user_id: string;
     nick: string;
