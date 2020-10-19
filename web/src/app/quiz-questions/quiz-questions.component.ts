11504bb56a7b0e44ecde828df61f7e15b671060c
ljxm <lucymurphy98@hotmail.com>
2020-10-19 12:51:21 +0100

    A basic layout for the main pages (#87)
    * A basic layout for the main pages
    
    a layout for register, quiz questions and score
    
    * node modules should be gone
    
    Co-authored-by: Lucy Murphy <lucymurphy@lucys-air.home>
    Co-authored-by: Dumitru Vulpe <dumitru.v.dv@gmail.com>

--- web/src/app/quiz-questions/quiz-questions.component.ts
+++ web/src/app/quiz-questions/quiz-questions.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { Question } from '../question';
+import { questionJson } from './question.mock';
+
+// tslint:disable: no-string-literal
 @Component({
   selector: 'app-quiz-questions',
   templateUrl: './quiz-questions.component.html',
-  styleUrls: ['./quiz-questions.component.css']
+  styleUrls: ['../app.component.css', './quiz-questions.component.css']
 })
 export class QuizQuestionsComponent implements OnInit {
-
+  title = 'Quiz';
+  question: Question =
+    {
+      qs: questionJson['question'],
+      ans1: questionJson['ans1'],
+      ans2: questionJson['ans2'],
+      ans3: questionJson['ans3'],
+      ans4: questionJson['ans4']
+    }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+export class ReadingJsonFilesComponent {
+
+  constructor() {
+    console.log('Reading local json files');
+    console.log(questionJson);
+  }
 }
