8a600baaaa8c6f90fa68ac2aaa5a66dd13272675
Christopher Carr <CYCarr@dundee.ac.uk>
2020-10-21 18:32:25 +0100

    Added answer buttons

--- web/src/app/quiz-questions/quiz-questions.component.ts
+++ web/src/app/quiz-questions/quiz-questions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { RestService } from '../services/rest.service';
+import { Question } from '../question';
 
 @Component({
   selector: 'app-quiz-questions',
@@ -22,4 +22,12 @@ export class QuizQuestionsComponent {
     var question = document.getElementById('q');
     question.innerHTML = q;
   }
+  question: Question =
+    {
+      qs: 'question',
+      ans1: 'ans1',
+      ans2: 'ans2',
+      ans3: 'ans3',
+      ans4: 'ans4'
+    }
 }
