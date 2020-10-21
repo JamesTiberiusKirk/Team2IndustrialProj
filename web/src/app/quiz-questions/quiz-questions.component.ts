58fae226e337acc3a560403c9db405afb6c6fc19
Christopher Carr <CYCarr@dundee.ac.uk>
2020-10-21 23:42:19 +0100

    questions read into buttons

--- web/src/app/quiz-questions/quiz-questions.component.ts
+++ web/src/app/quiz-questions/quiz-questions.component.ts
@@ -12,9 +12,23 @@ export class QuizQuestionsComponent {
 
   constructor(private router: Router, private rest: RestService) { }
 
+  question: Question =
+    {
+      qs: 'question',
+      ans1: 'Placeholder answer1',
+      ans2: 'Placeholder answer2',
+      ans3: 'Placeholder answer3',
+      ans4: 'Placeholder answer4'
+    }
   startQuiz(roomId: string, userId: string) {
     this.rest.getNextQuestion(roomId, userId).subscribe((data) => {
       this.setQ(data.question.text);
+      var a, b, c, d;
+      
+      
+      
+      
+      this.setAnswers(a = data.answers[0].text, b = data.answers[1].text, c = data.answers[2].text, d = data.answers[3].text);
     });
   }
 
@@ -22,12 +36,13 @@ export class QuizQuestionsComponent {
     var question = document.getElementById('q');
     question.innerHTML = q;
   }
-  question: Question =
-    {
-      qs: 'question',
-      ans1: 'ans1',
-      ans2: 'ans2',
-      ans3: 'ans3',
-      ans4: 'ans4'
-    }
+  setAnswers(a1: string, a2: string, a3: string, a4: string) {
+    //console.log("setting answers..")
+    document.getElementById('buttonA').innerHTML = a1;
+    document.getElementById('buttonB').innerHTML = a2;
+    document.getElementById('buttonC').innerHTML = a3;
+    document.getElementById('buttonD').innerHTML = a4;
+  }
+
+
 }
